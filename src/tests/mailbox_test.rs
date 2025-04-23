05e928af2aa1c6758fdc063be85a7e351b20fab8
Sebastian Mygind <sebmygind@gmail.com>
2025-04-23 23:28:25 +0200

    add some tests, continue work

--- src/tests/mailbox_test.rs
+++ src/tests/mailbox_test.rs
@@ -13,4 +13,48 @@ mod tests {
 
         assert_eq!(new_pos, Some(8));
     }
+
+    #[test]
+    fn test_legal_move_2() {
+        let current_position = 46;
+
+        let move_to_make = Move { dx: 1, dy: 2 };
+
+        let new_pos = get_new_position(current_position, move_to_make);
+
+        assert_eq!(new_pos, Some(63));
+    }
+
+    #[test]
+    fn test_illegal_move_1() {
+        let current_position = 0;
+
+        let move_to_make = Move { dx: -1, dy: 0 };
+
+        let new_pos = get_new_position(current_position, move_to_make);
+
+        assert_eq!(new_pos, None);
+    }
+
+    #[test]
+    fn test_illegal_move_2() {
+        let current_position = 0;
+
+        let move_to_make = Move { dx: 0, dy: -1 };
+
+        let new_pos = get_new_position(current_position, move_to_make);
+
+        assert_eq!(new_pos, None);
+    }
+
+    #[test]
+    fn test_illegal_move_3() {
+        let current_position = 63;
+
+        let move_to_make = Move { dx: 1, dy: 2 };
+
+        let new_pos = get_new_position(current_position, move_to_make);
+
+        assert_eq!(new_pos, None);
+    }
 }
