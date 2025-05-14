da7d2568381ed15733c58cdfeee9ff7d06ffcd2d
Sebastian Mygind <sebmygind@gmail.com>
2025-05-14 23:04:04 +0200

    Fix an import

--- src/tests/legal_move_test.rs
+++ src/tests/legal_move_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::chessboard::{self, ChessBoard, EMPTY, Players, WPAWN};
+    use crate::chessboard::{ChessBoard, EMPTY, Players, WPAWN};
     use crate::fen::Fen;
     use crate::moves::{LegalMove, MoveType};
 
