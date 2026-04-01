9ff66321090cba7c9324ce954204c83ed1eab117
Kristiyan <60707126+Danar435@users.noreply.github.com>
2026-04-01 13:53:34 +0200

    Fix

--- README.md
+++
@@ -1,2 +0,0 @@
-todo!
-- The pawn move generation is badly written and should be refactored, currently it also does not work correctly for enpassant moves.
--- src/engine/utils.rs
+++
@@ -1,5 +0,0 @@
-use crate::chessboard::Move;
-
-pub const fn fold_pawn_moves(single: Move, double: Move, attack: [Move; 2]) -> [Move; 4] {
-    [single, double, attack[0], attack[1]]
-}
